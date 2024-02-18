4f4c51041a1ee42c82e6860ec167d8e5b94c8a22
pretzelVillain <maxmenov@mail.ru>
2024-02-18 16:46:43 +0500

    Initial commit

---
+++ src/main/java/com/example/spring2024/Spring2024Application.java
@@ -0,0 +1,13 @@
+package com.example.spring2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2024Application.class, args);
+	}
+
+}
